e2e6ec08601d105e308c3fe1dda8f14dab05cccd
olgame2027 <olgame2027@gmail.com>
2022-02-26 09:04:43 +0300

    first

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        switch (args[0]) {
+            case "v0" -> v0();
+            case "exit" -> {
+                return;
+            }
+        }
+    }
+
+    private static void v0() {
+        System.out.println("llllllllllll");
+    }
+}
---
+++ src/main/java/Methtmp.java
@@ -0,0 +1,11 @@
+public class Methtmp {
+
+    public static void main(String[] args) {
+        System.out.println( sum(2, 3) );
+    }
+
+    public static double sum(double a, double b) {
+        return a + b;
+    }
+
+}
